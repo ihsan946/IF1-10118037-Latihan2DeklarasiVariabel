fc52bc026628d3ecc5b4bf98757bc6e30468fb33
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:24:32 +0700

    Add files via upload

---
+++ IF110118037Latihan2DeklarasiVariabel/src/if110118037latihan2deklarasivariabel/IF110118037Latihan2DeklarasiVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan2deklarasivariabel;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program mendeklarasikan variabel. 
+ */
+public class IF110118037Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai variabel
+        nilaiInt      = 90;
+        nilaiLogika   = false;
+        nilaiKarakter = 'A';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai\t= " + nilaiInt);
+        System.out.println("Isi variabel PHI\t= " + PHI);
+        System.out.println("Isi variabel logika\t= " + nilaiLogika);
+        System.out.println("Isi variabel Karakter\t= " + nilaiKarakter);
+    }
+    
+}
